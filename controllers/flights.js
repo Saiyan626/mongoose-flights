c3acdbb8420ec564d84d2911def41524d98f5330
Urian Muro <urianmuro@Urians-MacBook-Pro.local>
2021-03-22 14:41:38 -0700

    Part1-Commit

---
+++ controllers/flights.js
@@ -0,0 +1,29 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    new: newFlight,
+    create,
+    index
+};
+
+function index(req, res) {
+    Flight.find({}).sort({ departs: "ascending" }).exec(function (err, flights) {
+        res.render('flights/index', { flights });
+    });
+}
+
+function create(req, res) {
+    if (req.body.departs === '') delete req.body.departs;
+    Flight.create(req.body, function (err, flight) {
+        if (err) console.log(err);
+        res.redirect(`/flights`);
+    })
+}
+
+function newFlight(req, res) {
+    const newFlight = new Flight();
+    const dt = newFlight.departs;
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    res.render('flights/new', { departsDate });
+}
---
+++ routes/flights.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+router.get('/', flightsCtrl.index);
+/* GET /flights/new */
+router.get('/new', flightsCtrl.new);
+// POST/ flights
+router.post('/', flightsCtrl.create);
+
+module.exports = router;
